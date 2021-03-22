e30dfc2b5d47a8224c14335dff74ff4772a3b7db
Kwaku <kwakugyapong219@gmail.com>
2021-03-21 22:57:51 -0400

    First Deliverable

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listing, setListings]= useState([])
+useEffect (
+() => {
+    fetch("http://localhost:6001/listings")
+    .then(response => response.json())
+    .then(listing => {
+      setListings(listing)
+    })
+},[]
+
+)
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listing={listing} />
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,19 @@
 import React from "react";
+import ListingCard from "./ListingCard";
 // import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({listing}) {
+
+const craiglist= listing.map((list) => {
+return <ListingCard key= {list.id} description={list.description} image ={list.image} location ={list.location}/>
+
+})
+
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {craiglist}
       </ul>
     </main>
   );
